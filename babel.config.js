3859a44ad9c6504639887eb7a436054667720570
Jonathan Lancaster <lancast001@gmail.com>
2024-02-22 22:46:19 -0600

    install react native dotenv dep and update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: ["react-native-reanimated/plugin"],
+module.exports = function (api) {
+	api.cache(false);
+
+	const presets = ["module:metro-react-native-babel-preset"];
+	const plugins = [
+		["module:react-native-dotenv"],
+		"react-native-reanimated/plugin",
+	];
+
+	return { presets, plugins };
 };
